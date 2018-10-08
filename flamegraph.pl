50bfca3c4d33ef982c75f5b9e602eb8fa82abb5d
zhuzi <nmz89825@gmail.com>
2018-10-08 12:05:07 +0800

    change: tooltip text added percentage.

--- flamegraph.pl
+++ flamegraph.pl
@@ -797,7 +797,9 @@ my $inc = <<INC;
             .style("opacity", .9);
         tooltip.attr("x", box.x + 10)
 			.attr("y", box.y - 10)
-		document.getElementById("tooltip-text").textContent = info.replace(/\\([^(]*\\)\$/,"");
+		console.log(info);
+		document.getElementById("tooltip-text").textContent = info.replace(/\\((.\* samples,\\s)/, '(');
+
 	}
 	function c() {			// clear
 		details.nodeValue = ' ';
